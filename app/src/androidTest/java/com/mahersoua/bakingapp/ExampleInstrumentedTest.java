6fac5bff072755dd1bf8a28931d841a1c03d51eb
Maher Soua <mahersoua@gmail.com>
2018-09-06 01:12:03 +0200

    Add Espresso librairies, Start Instrument test.

--- app/src/androidTest/java/com/mahersoua/bakingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mahersoua/bakingapp/ExampleInstrumentedTest.java
@@ -2,11 +2,17 @@ package com.mahersoua.bakingapp;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +22,21 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    public static final String MAIN_ERROR_MESSAGE = "Green Tea";
+    @Rule
+    public ActivityTestRule< MainActivity> mActivityTestRule =
+            new ActivityTestRule(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.user.bakingapp", appContext.getPackageName());
+        assertEquals("com.mahersoua.bakingapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void startApplicationTest() {
+        onView(withId(R.id.connectionErrorTv)).check(matches(withText(mActivityTestRule.getActivity().getString(R.string.access_internet_error))));
     }
 }
